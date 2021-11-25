79ec7e10723db13001215230386f1736c6ee071f
mochuk <olena2001mochuk@gmail.com>
2021-11-25 20:49:00 +0200

    Update PARSER
    feat: function insert, contains and search

--- mochuk_fi-91_syryza_fi-91/src/parser11.py
+++ mochuk_fi-91_syryza_fi-91/src/parser11.py
@@ -18,11 +18,11 @@ def find_words(line):
     elif re.match(r'^[Ss][Ee][Aa][Rr][Cc][Hh]\b', arr_words[0]):
         search(arr_words, line)
     else:
-        print("Неправильна назва команди або назва команди відсутня взагалі")
+        print("Неправильна назва команди або назва команди відсутня взагалі\n")
 
 def create(arr_words):
     #дістаємо ім'я множини
-    name=arr_words[1]
+    name = arr_words[1]
     #перевіряємо правильність команди
     if (len(arr_words) == 2):
         #перевіряємо наявність цієї множини (співпадіння назви з одним із елементів масиву)
@@ -35,47 +35,51 @@ def create(arr_words):
             print("Множина", name, "була створена\n")
             print(arr_tree)
     else:
-        print("Неправильно введена команда 'CREATE'")
+        print("Неправильно введена команда 'CREATE'\n")
 
 def insert(arr_words, line):
-    k=0
-    m=0
+    k = 0
+    m = 0
+    temp1 = []
+    temp2 = []
     #дістаємо ім'я множини
     name = arr_words[1]
     #перевіряємо наявність цієї множини (співпадіння назви з одним із елементів масиву)
     if (name in names):
-        #перевіряємо правильність команди
-        if re.search(r'[,]', line):
-            #дістаємо значення x та у
-            a = re.split(r'[(]', line)
+        #перевіряємо правильність команди і дістаємо значення x та у
+        a = re.split(r'[(]', line)
+        N = re.findall(r'\w+', a[0])
+        if (len(N) == 2):
             b = re.split(r'[)]', a[1])
-            c = re.findall(r'\w', b[0])
+            c = re.findall(r'\S', b[0])
             for i in range(len(c)):
                 if (c[i] == ','):
-                    k = i-1
-                    return k
-            for j in range(1, k):
-                temp1 = []
+                    k = i
+            for j in range(0, k):
                 temp1.append(c[j])
-                return temp1
-            for l in range(k,len(c)):
-                temp2 = []
+            for l in range(k + 1,len(c)):
                 temp2.append(c[l])
-                return temp2
-            x = temp1.join('')
-            y = temp2.join('')
+            if(len(temp1) > 1):
+                x = int(''.join(temp1))
+            else:
+                x = int(temp1[0])
+            if(len(temp2) > 1):
+                y = int(''.join(temp2))
+            else:
+                y = int(temp2[0])
             #записуємо х та у
             for i in range(len(arr_tree)):
                 if (arr_tree[i][0] == name):
                     m = i
-                    return m
-            arr_tree[m].append([x, y])
-            print(arr_tree)
-            print("Точка (", x, ",", y, ") була додана до ", name, "\n")
+                    if (not arr_tree[i][1]):
+                        arr_tree[i][1] = ([x, y])
+                    else:
+                        arr_tree[m].append([x, y])
+            print("Точка (" + str(x) + ",", str(y) + ") була додана до", name, "\n")
         else:
-            print("Неправильно введена команда 'INSERT'")
+            print("Неправильно введена команда 'INSERT'\n")
     else:
-        print("Точка не була додана до ", name, ", бо множина з таким іменем не існує\n")
+        print("Точка не була додана до", name, ", бо множина з таким іменем не існує\n")
 
 def print_tree(arr_words):
     #дістаємо ім'я множини
@@ -93,51 +97,64 @@ def print_tree(arr_words):
                     print(arr_tree[i])
             #тут буде функція виведення структури дерева
     else:
-        print("Неправильно введена команда 'PRINT TREE'")
+        print("Неправильно введена команда 'PRINT_TREE'\n")
 
 def contains(arr_words, line):
+    k = 0
+    m = 0
+    w = 0
+    temp1 = []
+    temp2 = []
     #дістаємо ім'я множини
     name = arr_words[1]
     #перевіряємо наявність цієї множини (співпадіння назви з одним із елементів масиву)
     if (name in names):
-        #перевіряємо правильність команди
-        if re.search(r'[,]', line):
-            #дістаємо значення x та у
-            a = re.split(r'[(]', line)
+        #перевіряємо правильність команди і дістаємо значення x та у
+        a = re.split(r'[(]', line)
+        N = re.findall(r'\w+', a[0])
+        if (len(N) == 2):
             b = re.split(r'[)]', a[1])
-            c = re.findall(r'\w', b[0])
+            c = re.findall(r'\S', b[0])
             for i in range(len(c)):
                 if (c[i] == ','):
-                    k = i-1
-                    return k
-            for j in range(1, k):
-                temp1 = []
+                    k = i
+            for j in range(0, k):
                 temp1.append(c[j])
-                return temp1
-            for l in range(k,len(c)):
-                temp2 = []
+            for l in range(k + 1,len(c)):
                 temp2.append(c[l])
-                return temp2
-            x = temp1.join('')
-            y = temp2.join('')
+            if(len(temp1) > 1):
+                x = int(''.join(temp1))
+            else:
+                x = int(temp1[0])
+            if(len(temp2) > 1):
+                y = int(''.join(temp2))
+            else:
+                y = int(temp2[0])
             #шукаємо х та у
             for i in range(len(arr_tree)):
                 if (arr_tree[i][0] == name):
                     m = i
-                    return m
-            for j in range(len(arr_tree[m])):
-                if (arr_tree[m][j][0] == x):
-                    if (arr_tree[m][j][1] == x):
-                        print('True')
-                else:
-                    print('False')
+            for s in range(len(arr_tree[m])):
+                if (arr_tree[m][s][0] == x):
+                    if (arr_tree[m][s][1] == y):
+                        w+=1
+            if w == 1:
+                print("True\n")
+            else:
+                print("False\n")
         else:
-            print("Неправильно введена команда 'CONTAINS'")
+            print("Неправильно введена команда 'CONTAINS'\n")
     else:
-        print("Точка не була додана до ", name, ", бо множина з таким іменем не існує\n")
+        print("Точка не була додана до", name + ", бо множина з таким іменем не існує\n")
 
 def search(arr_words, line):
     k = 0
+    m = 0
+    n = 0
+    temp1 = []
+    temp2 = []
+    temp11 = []
+    temp12 = []
     #дістаємо ім'я множини
     name = arr_words[1]
     #перевіряємо правильність команди
@@ -151,88 +168,112 @@ def search(arr_words, line):
             for i in range(len(arr_tree)):
                 if (arr_tree[i][0] == name):
                     k = i
-                    return k
-            for j in arr_tree[k][1:]:
-                temp = []
-                temp[0] = arr_tree[k][j][0]
-                temp[1] = arr_tree[k][j][1]
-                print('(' + temp[0]+ ',', temp[1] + ')')
+            for j in range(1, len(arr_tree[k])):
+                print('(' + str(arr_tree[k][j][0]) + ',', str(arr_tree[k][j][1]) + ')')
     #перевіряємо правильність команди WHERE
     elif re.match(r'^[Ww][Hh][Ee][Rr][Ee]\b', arr_words[2]):
         if (len(arr_words) == 5):
             #дістаємо значення y
-            y = arr_words[4]
+            y = int(arr_words[4])
             #перевіряємо правильність команди ABOVE_TO
-            if re.match(r'^[Aa][Bb][Oo][Vv][Ee][_][Tt][Oo]', arr_words[3]):
+            if re.match(r'^[Aa][Bb][Oo][Vv][Ee][_][Tt][Oo]\b', arr_words[3]):
                 #перевіряємо наявність цієї множини (співпадіння назви з одним із елементів масиву)
                 if (name not in names):
                     print("Пошук точок у множині", name, "не був проведений, бо множина з таким іменем не існує\n")
                     print(names)
                 else:
-                    print("Всі точки із множини", name, "координата y, яких більша за", у, "будуть виведені\n")
+                    print("Всі точки із множини", name, "координата y, яких більша за", y, "будуть виведені\n")
                     for i in range(len(arr_tree)):
-                        if (array[i][0] == name):
+                        if (arr_tree[i][0] == name):
                             k = i
-                            return k
-                    for j in range(len(arr_tree[k])):
+                    for j in range(1, len(arr_tree[k])):
                         if (arr_tree[k][j][1] > y):
-                            print(arr_tree[k][j])
+                            print("(" + str(arr_tree[k][j][0]) + ",", str(arr_tree[k][j][1]) + ")")
             else:
                 print("Неправильно введена команда 'ABOVE_TO'")
         #перевіряємо правильність команди NN
         elif re.match(r'^[Nn][Nn]\b', arr_words[3]):
-            #перевіряємо правильність команди
-            if re.search(r'[,]', line):
-                #дістаємо значення x та у
-                a = re.split(r'[(]', line)
+            #перевіряємо правильність команди і дістаємо значення x та у
+            a = re.split(r'[(]', line)
+            N = re.findall(r'\w+', a[0])
+            if (len(N) == 4):
                 b = re.split(r'[)]', a[1])
-                c = re.findall(r'\w', b[0])
+                c = re.findall(r'\S', b[0])
                 for i in range(len(c)):
                     if (c[i] == ','):
-                        k = i-1
-                        return k
-                for j in range(1, k):
-                    temp1 = []
+                        k = i
+                for j in range(0, k):
                     temp1.append(c[j])
-                return temp1
-                for l in range(k,len(c)):
-                    temp2 = []
+                for l in range(k + 1,len(c)):
                     temp2.append(c[l])
-                    return temp2
-                    x = temp1.join('')
-                    y = temp2.join('')
-                print("Найближчий сусід точки (", x, ",", y, ") точка ...")
+                if(len(temp1) > 1):
+                    x = int(''.join(temp1))
+                else:
+                    x = int(temp1[0])
+                if(len(temp2) > 1):
+                    y = int(''.join(temp2))
+                else:
+                    y = int(temp2[0])
+                #тут буде функція пошуку найближчого сусіда
+                print("Найближчий сусід точки (" + str(x) + ",", str(y) + ") точка ...\n")
             else:
-                print("Неправильно введена команда 'NN'")
+                print("Неправильно введена команда 'NN'\n")
         #перевіряємо правильність команди INSIDE
         elif re.match(r'^[Ii][Nn][Ss][Ii][Dd][Ee]\b', arr_words[3]):
-            #перевіряємо правильність команди
-            #if re.search(r'[,]', line):
-                #дістаємо значення x та у
-                #a = re.split(r'[(]', line)
-                #b = re.split(r'[)]', a[1])
-                #c = re.findall(r'\w', b[0])
-                #for i in range(len(c)):
-                    #if (c[i] == ','):
-                        #k = i-1
-                        #return k
-                #for j in range(1, k):
-                    #temp1 = []
-                    #temp1.append(c[j])
-                #return temp1
-                #for l in range(k,len(c)):
-                    #temp2 = []
-                    #temp2.append(c[l])
-                    #return temp2
-                    #x = temp1.join('')
-                    #y = temp2.join('')
-                #print("Найближчий сусід точки (", x, ",", y, ") точка ...")
-                print("прямокутник...")
-            #else:
-                #print("Неправильно введена команда 'INSIDE'")
+            #перевіряємо правильність команди і дістаємо значення x та у
+            a = re.split(r'[(]', line)
+            N = re.findall(r'\w+', a[0])
+            if (len(N) == 4):
+                b = re.split(r'[)]', a[1])
+                c = re.findall(r'\S', b[0])
+                d = re.findall(r'\S', b[1])
+                e = re.split(r'[)]', a[2])
+                f = re.findall(r'\S', b[0])
+                #перевіряємо наявність коми між точками
+                if (d[0] == ','):
+                    #дістаємо значення x та у нижнього лівого кута
+                    for i in range(len(c)):
+                        if (c[i] == ','):
+                            k = i
+                    for j in range(0, k):
+                        temp1.append(c[j])
+                    for l in range(k + 1,len(c)):
+                        temp2.append(c[l])
+                    if(len(temp1) > 1):
+                        x1 = int(''.join(temp1))
+                    else:
+                        x1 = int(temp1[0])
+                    if(len(temp2) > 1):
+                        y1 = int(''.join(temp2))
+                    else:
+                        y1 = int(temp2[0])
+                    #дістаємо значення x та у верхнього правого кута
+                    for i in range(len(f)):
+                        if (f[i] == ','):
+                            n = i
+                    for j in range(0, n):
+                        temp11.append(f[j])
+                    for l in range(n + 1,len(f)):
+                        temp12.append(f[l])
+                    if(len(temp11) > 1):
+                        x2 = int(''.join(temp11))
+                    else:
+                        x2 = int(temp11[0])
+                    if(len(temp12) > 1):
+                        y2 = int(''.join(temp12))
+                    else:
+                        y2 = int(temp12[0])
+                    #тут буде функція пошуку точок у прямокутнику
+                    print("Точки, які містяться всередині або лежать на сторонах прямокутника, який задано координатами його вершин: (" + str(x1) + ",", str(y1) + ") - координати нижнього лівого кута, (" + str(x2) + ",", str(y2) + ") - координати верхнього правого кута: ...\n")
+                else:
+                    print("Неправильно введена команда 'INSIDE'\n")
+            else:
+                print("Неправильно введена команда 'INSIDE'\n")
+        else:
+            print ("Неправильно введена команда 'SEARCH'\n")
     else:
-        print ("Неправильно введена команда 'SEARCH'")
+        print ("Неправильно введена команда 'SEARCH'\n")
 
 while True:
-    line = input()
+    line = input('---> ')
     find_words(line)
